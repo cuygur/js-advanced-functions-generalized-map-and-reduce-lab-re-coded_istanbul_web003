c62f2c9a9653ac83555cb6afed33a15b552b12d5
Cihan_Uygur <magicihan89@gmail.com>
2020-11-01 02:48:24 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+const map = (arr, func) => {
+  const result = []
+  for (let element of arr) {
+    result.push(func(element))
+  }
+  return result
+}
+
+const reduce = (array, func, start) => {
+  if (!!start == false) {
+    start = array.shift()
+  }
+  return array.reduce(func, start)
+}
